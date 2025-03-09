0064bee73b8475c73223a379e6aca9cfb900cd6f
underscore-chil <seawaysdeliveryinc@yahoo.com>
2025-03-09 14:33:48 +0100

    Initial commit.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,46 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {
+  ApplicationConfig,
+  provideExperimentalZonelessChangeDetection,
+  provideZoneChangeDetection,
+} from '@angular/core';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 
 import { routes } from './app.routes';
+import {
+  provideClientHydration,
+  withEventReplay,
+} from '@angular/platform-browser';
+import {
+  provideHttpClient,
+  withFetch,
+  withInterceptors,
+} from '@angular/common/http';
+import { provideToastr } from 'ngx-toastr';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { urlInterceptor } from './interceptors/url.interceptor';
+import { tokenInterceptor } from './interceptors/token.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    // provideZoneChangeDetection({ eventCoalescing: true }),
+    provideExperimentalZonelessChangeDetection(),
+    provideRouter(
+      routes,
+      withInMemoryScrolling({
+        anchorScrolling: 'enabled',
+        scrollPositionRestoration: 'enabled',
+      })
+    ),
+    provideClientHydration(withEventReplay()),
+    provideHttpClient(
+      withInterceptors([urlInterceptor, tokenInterceptor]),
+      withFetch()
+    ),
+    provideAnimationsAsync(),
+    provideToastr({
+      preventDuplicates: true,
+      timeOut: 4000,
+      positionClass: 'toast-top-right',
+    }),
+  ],
 };
