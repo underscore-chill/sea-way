6a88515b7110fa65cabc3ada63fa49344d185815
underscore-chil <seawaysdeliveryinc@yahoo.com>
2025-03-22 11:14:54 +0100

    Pricing button.

--- src/app/user/pricing/pricing.component.ts
+++ src/app/user/pricing/pricing.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { apiConfig } from '../../config/api.config';
 
 @Component({
   selector: 'app-pricing',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './pricing.component.html',
-  styleUrl: './pricing.component.css'
+  styleUrl: './pricing.component.css',
 })
 export class PricingComponent {
-
+  whatsappNumber = signal<string>(apiConfig.whatsappNumber);
 }
